9361a83bf0e8e46de268c67d33b7b44aa0f8479f
ShadowBlack2789 <36540934+ShadowBlack2789@users.noreply.github.com>
2018-10-11 17:26:10 -0400

    upload
    This is the model from lab1

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Lab1B.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Company")]
+        public string Company { get; set; }
+
+        [Display(Name = "Position")]
+        public string Position { get; set; }
+
+        [Required]
+        [Display(Name = "Birthday")]
+        [DataType(DataType.Date)]
+        public string Birthday { get; set; }
+    }
+}
---
+++ Models/Dealership.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1B.Models
+{
+    public class Dealership
+    {
+        [Key]
+        public string ID { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name="Phone")]
+        public int PhoneNumber { get; set; }
+    }
+}
